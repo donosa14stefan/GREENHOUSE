55b3222c9bdd03362e1e5e8700ed88ada1279567
Stefan Donosa <161770387+donosa14stefan@users.noreply.github.com>
2024-10-26 23:58:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
 #include <QDesktopWidget>
-
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    // Force using offscreen rendering if X11 is not available
+    qputenv("QT_QPA_PLATFORM", "offscreen");
+    
     QApplication app(argc, argv);
     MainWindow window;
     window.show();
